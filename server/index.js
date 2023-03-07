b2f8303256c8333dab63c8029876d27a06d53ace
Winter Soldier <eshwaripreetham790@psnacet.edu.in>
2023-03-07 21:40:51 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postROutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({limit:"50mb"}));
+
+app.use("/api/v1/post",postRoutes);
+app.use("/api/v1/dalle",dalleRoutes);
+
+app.get("/",async (req,res) =>{
+    res.send("Hello from DALL-E!");
+});
+
+
+const startServer = async () =>{
+    try{
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080),()=> console.log("listening on port 8080");
+    } catch(err){
+        console.log(err);
+    }
+    
+}
+
+startServer();
